93137d5bb4d003defb56fb67fea38eb1037ded1e
Praduman Prajapati <tech.praduman@gmail.com>
2023-02-24 19:24:42 +0530

    Add files via upload

---
+++ complete web development Bootcaps/tut53.js
@@ -0,0 +1,14 @@
+console.log("this is titorial");
+
+function greet(name){
+    console.log(name + "is a good boy");
+}
+
+let name = "harry";
+let name1 = "shubham";
+let name2 = "vipul";
+let name3 = "rohan";
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
